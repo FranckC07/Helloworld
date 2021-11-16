298836a2202d3d4e4ee3704af5e06eaa280806e3
FranckC07 <franck.chassier@gmail.com>
2021-11-16 19:13:48 +0100

    Exercice 1-XAML Essentials  + comparaison correction

--- Helloworld/MainPage.xaml.cs
+++ Helloworld/MainPage.xaml.cs
@@ -10,16 +10,51 @@ namespace Helloworld
 {
     public partial class MainPage : ContentPage
     {
+        static int clic = 0;// ******************* ME 
+        private int _index = 0;
+        private string[] _quotes = new string[]
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
         public MainPage()
         {
             InitializeComponent();
+            currentQuote.Text = _quotes[_index];
+           
 
         }
-        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        void Handle_Clicked(object sender, System.EventArgs e)
         {
-            Label.Text = String.Format("Value is {0}", e.NewValue);
+            _index++;
+            if (_index >= _quotes.Length)
+                _index = 0;
 
+            currentQuote.Text = _quotes[_index];
+        }
+        //********************* ME *********************************
+        void next_Clicked(object sender, System.EventArgs e)
+        {
+            if (clic == 0)
+            {
+                text.Text = "Text de seconde position";
+            };
+            if (clic == 1)
+            {
+                text.Text = "Text de troisième position";
+            }
+            if (clic == 2)
+            {
+                text.Text = "Text on vas boucler apres cela";
+                clic = 0;
+            }
+            else
+            {
+                clic++;
+            }
         }
 
     }
 }
+
